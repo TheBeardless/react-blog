0f8c8970f3d46947c038ca34e62f1ea5169cf421
thebeardless <jakepeterwebster@gmail.com>
2021-03-14 14:18:25 +1000

    created routes and an abort controller

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,11 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
-  useEffect(() => {
-    setTimeout(() => {
-      // fake loading for testing
-      fetch("http://localhost:8000/blogs")
-        .then((response) => {
-          if (!response.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []); // <- dependencies that render on change
-
-  //conditional template - if blogs == null then don't load BlogList.
   return (
     <div className="home">
       {error && <div>{error}</div>}
